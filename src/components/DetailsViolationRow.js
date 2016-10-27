19d9bec4f472fe95ca2a1213437d662f08c80615
Alex Aragon <Alex.Aragon@kingcounty.gov>
2016-10-27 10:52:45 -0700

    Removing bad char symbols from inspection strings

--- src/components/DetailsViolationRow.js
+++ src/components/DetailsViolationRow.js
@@ -1,10 +1,10 @@
 import React, {PropTypes} from 'react';
 
 const DetailsInspectionRow = ({violation, index}) => {
-
+  //http://stackoverflow.com/questions/4374822/javascript-regexp-remove-all-special-characters
   return (
     <tr className={"collapse " + index} key={index}>
-      <td colSpan="2" className="p-l-lg">{violation.violationDescr} </td>
+      <td colSpan="2" className="p-l-lg">{violation.violationDescr.replace(/[^\w\s]/gi, '')} </td>
       <td colSpan="1">{violation.violationPoints}</td>
     </tr>
   );
