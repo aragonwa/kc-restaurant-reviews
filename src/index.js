9a784f82efd795decfc081f9129d2645051f889b
Alex Aragon <alexaragon@MacBook-Pro-2.local>
2016-10-30 20:03:26 -0700

    Formated fixes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 /* eslint-disable import/default */
 import React from 'react';
 import {render} from 'react-dom';
-import { Provider } from 'react-redux';
-import { Router, browserHistory } from 'react-router';
+import {Provider} from 'react-redux';
+import {Router, browserHistory} from 'react-router';
 import routes from './routes';
 import {loadRestaurants} from './actions/restaurantReviewsActions';
 import configureStore from './store/configureStore';
 require('./favicon.ico'); // Tell webpack to load favicon.ico
 import './styles/styles.css'; // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
-import { syncHistoryWithStore } from 'react-router-redux';
+import {syncHistoryWithStore} from 'react-router-redux';
 
 const store = configureStore();
 
@@ -21,7 +21,7 @@ const history = syncHistoryWithStore(browserHistory, store);
 
 render(
   <Provider store={store}>
-    <Router history={history} routes={routes} />
+    <Router history={history} routes={routes}/>
   </Provider>, document.getElementById('app')
 );
 
