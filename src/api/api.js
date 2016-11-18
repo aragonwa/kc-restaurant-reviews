e419641ea80c213f8783ce40bfb28d496fd3acf6
Alex Aragon <aragonwa@gmail.com>
2016-11-18 09:11:13 -0800

    Testing polyfills for promise, fetch

--- src/api/api.js
+++ src/api/api.js
@@ -1,41 +1,41 @@
 export function getRestaurantsApi () {
   const url = '//kcit-bzzqfx1:40001/api/business/a/47.0845/-122.5284/47.7803/-121.0657';
   return new Promise(function (resolve, reject) {
-    fetch(url)
-      .then(response => {
-        if(response.ok) {
-          return resolve(response.json());
-        }
-        return Promise.reject(Error(response.status));
-      })
-      .catch((err) => {
-        return reject(Error('Network Error: ' + err));
-      });
+    // fetch(url)
+    //   .then(response => {
+    //     if(response.ok) {
+    //       return resolve(response.json());
+    //     }
+    //     return Promise.reject(Error(response.status));
+    //   })
+    //   .catch((err) => {
+    //     return reject(Error('Network Error: ' + err));
+    //   });
 
-  //   // Do the usual XHR stuff
-  //   let req = new XMLHttpRequest();
-  //   req.open('GET', url);
+    // Do the usual XHR stuff
+    let req = new XMLHttpRequest();
+    req.open('GET', url);
 
-  //   req.onload = function () {
-  //     // This is called even on 404 etc
-  //     // so check the status
-  //     if (req.status == 200) {
-  //       // Resolve the promise with the response text
-  //       resolve(req.response);
-  //     }else {
-  //       // Otherwise reject with the status text
-  //       // which will hopefully be a meaningful error
-  //       reject(Error(req.statusText));
-  //     }
-  //   };
+    req.onload = function () {
+      // This is called even on 404 etc
+      // so check the status
+      if (req.status == 200) {
+        // Resolve the promise with the response text
+        resolve(JSON.parse(req.response));
+      }else {
+        // Otherwise reject with the status text
+        // which will hopefully be a meaningful error
+        reject(Error(req.statusText));
+      }
+    };
 
-  //   // Handle network errors
-  //   req.onerror = function () {
-  //     reject(Error('Network Error'));
-  //   };
+    // Handle network errors
+    req.onerror = function () {
+      reject(Error('Network Error'));
+    };
 
-  //   // Make the request
-  //   req.send();
+    // Make the request
+    req.send();
  });
 }
 
