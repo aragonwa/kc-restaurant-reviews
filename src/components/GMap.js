6f4453264a192d3ea68555ff699e718fcecd8994
Alex Aragon <aragonwa@gmail.com>
2016-11-18 09:10:05 -0800

    Updating Map component

--- src/components/GMap.js
+++ src/components/GMap.js
@@ -1,59 +1,34 @@
 import React, {PropTypes} from 'react';
 import {Gmaps, Marker} from 'react-gmaps';
 
-//https://github.com/MicheleBertoli/react-gmaps/issues/39
-//Make map 2 components: http://stackoverflow.com/questions/32474475/react-update-children-but-not-immediate-parent
+//https://github.com/MicheleBertoli/react-gmaps
+
+const params = { v: '3.exp', key: 'AIzaSyDHJbH9ajNAa3hm7Sl5l3TklpGSB5by4mA' };
 
 class GMap extends React.Component {
   constructor(props, context) {
     super(props, context);
-    // this.state = {
-    //   lat:,
-    //   lng:,
-    //   zoom: 12
-    // }
     this.onMapCreated = this.onMapCreated.bind(this);
     this.onMarkerClick = this.onMarkerClick.bind(this);
   }
 
-
-  // shouldComponentUpdate(nextProps) {
-  //   if(this.props.pagerNum === nextProps.pagerNum && this.props.filter === nextProps.filter){
-  //     return false;
-  //   }
-  //   return true;
-  // }
-
-  // componentWillReceiveProps(nextProps) {
-
-  // }
-
-
-//On selection
-//
   componentDidUpdate(prevProps) {
-    //TODO: Use this to control the render https://facebook.github.io/react/docs/react-component.html#componentdidmount
     const {restaurants} = this.props;
     const {map} = this.state;
 
     if(restaurants.length === 0) {
       return;
     }
-    //if(this.props.pagerNum !== prevProps.pagerNum) {
-    const zoom = this.refs.gmaps.getMap().getZoom();
-    const center = map.getCenter();
-    console.log(this.refs.gmaps.getMap().center.lat);
 
-    const bounds = new google.maps.LatLngBounds();
-    restaurants.forEach((restaurant) => {
-      const lat = restaurant.businessLocationLat;
-      const lng = restaurant.businesssLocationLong;
-      bounds.extend(new google.maps.LatLng(lat,lng));
-    });
-    map.fitBounds(bounds);
-    map.setZoom(zoom);
-    //console.log(this.refs.gmaps.getMap().getZoom());
-   //}
+    if(this.props.pagerNum !== prevProps.pagerNum) {
+      const bounds = new google.maps.LatLngBounds();
+      restaurants.forEach((restaurant) => {
+        const lat = restaurant.businessLocationLat;
+        const lng = restaurant.businesssLocationLong;
+        bounds.extend(new google.maps.LatLng(lat,lng));
+      });
+      map.fitBounds(bounds);
+   }
   }
 
   onMapCreated(map) {
@@ -75,12 +50,7 @@ class GMap extends React.Component {
     });
 
     map.fitBounds(bounds);
-
   }
-
-  // onBoundsChanged(){
-
-  // }
 
   renderMarkers() {
     const {restaurants} = this.props;
@@ -110,23 +80,10 @@ class GMap extends React.Component {
     );
   }
 
-  // handle() {
-  //   //console.log('zoom:', this.refs.gmaps.getMap().getZoom(), this.refs.gmaps.getMap().getCenter().lat());
-  // }
-
   onMarkerClick(id) {
-    //TODO: map.setCenter(marker lat lng);
-
-    // this.setState({
-    //   zoom: map.getZoom(),
-    //   lat: map.getCenter().lat(),
-    //   lng: map.getCenter().lng()
-    // });
     this.props.setActiveItem(id);
   }
 
-  //https://github.com/MicheleBertoli/react-gmaps/issues/47
-  //https://github.com/MicheleBertoli/react-gmaps/issues/40
   render() {
     const {restaurants} = this.props;
     let height = ($(window).width() < 768) ? '300px' : '600px';
@@ -139,8 +96,9 @@ class GMap extends React.Component {
         <Gmaps
           ref="gmaps"
           height={height}
-          params={{v: '3.exp', key: 'AIzaSyDHJbH9ajNAa3hm7Sl5l3TklpGSB5by4mA'}}
-          onMapCreated={this.onMapCreated}>
+          onMapCreated={this.onMapCreated}
+          zoom={14}
+          params={params}>
           {this.renderMarkers()}
           </Gmaps>
       </div>
@@ -150,7 +108,10 @@ class GMap extends React.Component {
 
 GMap.propTypes = {
   restaurants: PropTypes.array.isRequired,
-  filter: PropTypes.string
+  filter: PropTypes.string,
+  activeItem: PropTypes.string,
+  pagerNum: PropTypes.number,
+  setActiveItem: PropTypes.func.isRequired
 };
 
 export default GMap;
