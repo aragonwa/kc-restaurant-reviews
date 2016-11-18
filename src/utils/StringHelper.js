2a799c644bf52d4b66ac1ee50bdb2148cd86c805
Alex Aragon <aragonwa@gmail.com>
2016-11-18 12:28:05 -0800

    Fixed bug in captialCase

--- src/utils/StringHelper.js
+++ src/utils/StringHelper.js
@@ -3,6 +3,7 @@ export default class StringHelper {
   static capitalCase (str) {
     // return str.toLowerCase().replace(/\b[a-z]/g,function(char) { return char.toUpperCase();});
     return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+    //return str.toLowerCase().replace(/(\b[a-z](?!\s))/g, function(txt){return txt.toUpperCase()});
   }
   static phoneNumFormat (str) {
     if(str) {
