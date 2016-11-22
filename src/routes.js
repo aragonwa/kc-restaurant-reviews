06fe0e747eaba1f6885fa34c36b29feb637c57c9
Alex Aragon <Alex.Aragon@kingcounty.gov>
2016-11-22 13:34:54 -0800

    added production environment variable

--- src/routes.js
+++ src/routes.js
@@ -6,17 +6,17 @@ import RestaurantReviewsPage from './containers/RestaurantReviewsPage'; // eslin
 import Details from './components/Details';
 
 //Todo: not found page <Route path="*" component={NotFoundPage}/>
-// let baseDir = '';
-// if(process.env.NODE_ENV === 'production') {
-//   baseDir = '/help/test/alex/restaurant-app';
-// }
+let baseDir = '';
+if(process.env.NODE_ENV === 'production') {
+  baseDir = '/help/test/alex/restaurant-app/#';
+}
 
 export default (
-  <Route path="/" component={App}>
+  <Route path={baseDir + '/'} component={App}>
     <IndexRoute component={RestaurantReviewsPage}/>
     <Route path="/" component={RestaurantReviewsPage}>
       <Redirect from="/details/" to="/" />
-      <Route path="/details/:id" component={Details} />
+      <Route path={baseDir + '/details/:id'} component={Details} />
     </Route>
     <Route path="*" component={RestaurantReviewsPage}/>
   </Route>
