6cd92fae05e5419253389bd5c2d101f0cc2c3577
Alex Aragon <Alex.Aragon@kingcounty.gov>
2017-01-31 14:51:01 -0800

    Setup RestaruantURL generator

--- src/constants/appSettings.js
+++ src/constants/appSettings.js
@@ -1,1 +1,27 @@
 export const PAGER_ITEMS_PER_PAGE = 10;
+
+export default class RestaurantUrls {
+  static setEnvironment (environment) {
+    let environmentUrl;
+
+    switch (environment) {
+      case 'staging':
+        environmentUrl = 'webstaging.kingcounty.gov/info.kingcounty.gov';
+        break;
+      case 'qa':
+        environmentUrl = 'infoqa.kingcounty.gov';
+        break;
+      case 'prod':
+        environmentUrl = 'info.kingcounty.gov';
+        break;
+    }
+
+    const apiUrls = {
+      fullList: '//' + environmentUrl + '/healthservices/RestaurantInspectionsPublicAPI/api/business/1/47.0845/-122.5284/47.7803/-121.0657/',
+      business: '//' + environmentUrl + '/healthservices/RestaurantInspectionsPublicAPI/api/business/',
+      inspections: '//' + environmentUrl + '/healthservices/RestaurantInspectionsPublicAPI/api/inspections/'
+    };
+
+    return apiUrls;
+  }
+}
