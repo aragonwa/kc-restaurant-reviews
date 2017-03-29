c88470ad817d101c2597b5fbb98a391f0ae40580
Alex Aragon <Alex.Aragon@kingcounty.gov>
2017-03-29 11:49:31 -0700

    Added route for search/:searchTerm

--- src/routes.js
+++ src/routes.js
@@ -8,12 +8,13 @@ import Details from './components/Details';
 //Todo: not found page <Route path="*" component={NotFoundPage}/>
 const baseDir = (process.env.NODE_ENV === 'production') ? '/depts/health/environmental-health/food-safety/inspection-system/search#' :'';
 export default (
-  <Route path={baseDir + '/'} component={App}>
-    <IndexRoute component={RestaurantReviewsPage}/>
-    <Route path="/" component={RestaurantReviewsPage}>
-      <Redirect from="/details/" to="/" />
-      <Route path={'/details/:id'} component={Details} />
-    </Route>
-    <Route path="*" component={RestaurantReviewsPage}/>
+<Route path={baseDir + '/'} component={App}>
+  <IndexRoute component={RestaurantReviewsPage}/>
+  <Route path="/" component={RestaurantReviewsPage}>
+    <Redirect from="/details/" to="/" />
+    <Route path={'/details/:id'} component={Details} />
   </Route>
+  <Route path="/search/:searchTerm" component={RestaurantReviewsPage} />
+  <Route path="*" component={RestaurantReviewsPage}/>
+</Route>
 );
