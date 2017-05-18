ad5bddebb2695a9ffaf6230fc619597928ec43a1
Alex Aragon <Alex.Aragon@kingcounty.gov>
2017-05-18 16:24:13 -0700

    Converting apis to use fetch

--- src/api/api.js
+++ src/api/api.js
@@ -5,115 +5,137 @@ const urlApi = RestaurantUrls.setEnvironment(process.env.NODE_ENV);
 export function getRestaurantsApi () {
   const url = urlApi.fullList;
 
-  return new Promise(function (resolve, reject) {
-    // fetch(url)
-    //   .then(response => {
-    //     if(response.ok) {
-    //       return resolve(response.json());
-    //     }
-    //     return Promise.reject(Error(response.status));
-    //   })
-    //   .catch((err) => {
-    //     return reject(Error('Network Error: ' + err));
-    //   });
+  // return new Promise(function (resolve, reject) {
+  return fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        return Promise.reject(Error(response.status));
+      }
+    })
+    .then(data => {
+      return Promise.resolve(data);
+    })
+    .catch(err => {
+      return Promise.reject(Error('Network Error: ' + err));
+    });
 
     // Do the usual XHR stuff
-    let req = new XMLHttpRequest();
-    req.open('GET', url);
+    // let req = new XMLHttpRequest()
+    // req.open('GET', url)
 
-    req.onload = function () {
-      // This is called even on 404 etc
-      // so check the status
-      if (req.status == 200) {
-        // Resolve the promise with the response text
-        resolve(JSON.parse(req.response));
-      }else {
-        // Otherwise reject with the status text
-        // which will hopefully be a meaningful error
-        reject(Error(req.statusText));
-      }
-    };
+    // req.onload = function () {
+    //   // This is called even on 404 etc
+    //   // so check the status
+    //   if (req.status == 200) {
+    //     // Resolve the promise with the response text
+    //     resolve(JSON.parse(req.response))
+    //   }else {
+    //     // Otherwise reject with the status text
+    //     // which will hopefully be a meaningful error
+    //     reject(Error(req.statusText))
+    //   }
+    // }
 
-    // Handle network errors
-    req.onerror = function () {
-      reject(Error('Network Error'));
-    };
+    // // Handle network errors
+    // req.onerror = function () {
+    //   reject(Error('Network Error'))
+    // }
 
-    // Make the request
-    req.send();
- });
+// // Make the request
+// req.send()
+// })
 }
 
 export function getBusinessApi (id) {
-   const url = urlApi.business + id;
+  const url = urlApi.business + id;
 
-  return new Promise(function (resolve, reject) {
-  //   fetch(url)
-  //     .then(response => {
-  //       if(response.ok) {
-  //         return resolve(response.json());
-  //       }
-  //       return Promise.reject(Error(response.status));
-  //     })
-  //     .catch((err) => {
-  //       return reject(Error('Network Error: ' + err));
-  //     });
-   // Do the usual XHR stuff
-    let req = new XMLHttpRequest();
-    req.open('GET', url);
-
-    req.onload = function () {
-      // This is called even on 404 etc
-      // so check the status
-      if (req.status == 200) {
-        // Resolve the promise with the response text
-        resolve(JSON.parse(req.response));
-      }else {
-        // Otherwise reject with the status text
-        // which will hopefully be a meaningful error
-        reject(Error(req.statusText));
+  // return new Promise(function (resolve, reject) {
+  return fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        return Promise.reject(Error(response.status));
       }
-    };
+    })
+    .then(data => {
+      return Promise.resolve(data);
+    })
+    .catch((err) => {
+      return Promise.reject(Error('Network Error: ' + err));
+    });
+    // Do the usual XHR stuff
+    //   let req = new XMLHttpRequest()
+    //   req.open('GET', url)
 
-    // Handle network errors
-    req.onerror = function () {
-      reject(Error('Network Error'));
-    };
+    //   req.onload = function () {
+    //     // This is called even on 404 etc
+    //     // so check the status
+    //     if (req.status == 200) {
+    //       // Resolve the promise with the response text
+    //       resolve(JSON.parse(req.response))
+    //     }else {
+    //       // Otherwise reject with the status text
+    //       // which will hopefully be a meaningful error
+    //       reject(Error(req.statusText))
+    //     }
+    //   }
 
-    // Make the request
-    req.send();
-  });
-  //.catch( e =>{Error(e.statusText);});
-}
+    //   // Handle network errors
+    //   req.onerror = function () {
+    //     reject(Error('Network Error'))
+    //   }
+
+//   // Make the request
+//   req.send()
+// })
+// .catch( e =>{Error(e.statusText);})
+};
 
 export function getInspectionsApi (id) {
   const url = urlApi.inspections + id;
 
-  return new Promise(function (resolve, reject) {
-    // Do the usual XHR stuff
-    let req = new XMLHttpRequest();
-    req.open('GET', url);
-
-    req.onload = function () {
-      // This is called even on 404 etc
-      // so check the status
-      if (req.status == 200) {
-        // Resolve the promise with the response text
-        resolve(JSON.parse(req.response));
-      }else {
-        // Otherwise reject with the status text
-        // which will hopefully be a meaningful error
-        reject(Error(req.statusText));
+  return fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        return Promise.reject(Error(response.status));
       }
-    };
+    })
+    .then(data => {
+      return Promise.resolve(data);
+    })
+    .catch(err => {
+      return Promise.reject(Error('Network Error: ' + err));
+    });
 
-    // Handle network errors
-    req.onerror = function () {
-      reject(Error('Network Error'));
-    };
+  // return new Promise(function (resolve, reject) {
+  //   // Do the usual XHR stuff
+  //   let req = new XMLHttpRequest();
+  //   req.open('GET', url);
 
-    // Make the request
-    req.send();
-  });
-}
+  //   req.onload = function () {
+  //     // This is called even on 404 etc
+  //     // so check the status
+  //     if (req.status == 200) {
+  //       // Resolve the promise with the response text
+  //       resolve(JSON.parse(req.response));
+  //     }else {
+  //       // Otherwise reject with the status text
+  //       // which will hopefully be a meaningful error
+  //       reject(Error(req.statusText));
+  //     }
+  //   };
 
+  //   // Handle network errors
+  //   req.onerror = function () {
+  //     reject(Error('Network Error'));
+  //   };
+
+  //   // Make the request
+  //   req.send();
+  // });
+};
