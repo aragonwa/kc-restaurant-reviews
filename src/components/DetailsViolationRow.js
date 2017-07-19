8d8b1085a8d378314454a3e7e5d266c34aca8414
Alex Aragon <Alex.Aragon@kingcounty.gov>
2017-07-19 15:49:19 -0700

    Critical violations are bold

--- src/components/DetailsViolationRow.js
+++ src/components/DetailsViolationRow.js
@@ -4,16 +4,17 @@ const DetailsInspectionRow = ({violation, index}) => {
   //http://stackoverflow.com/questions/4374822/javascript-regexp-remove-all-special-characters
   const {violationType} = violation;
   let violationIcon = null;
-
+  let violationStr = violation.violationDescr.replace(/[^\w\s]/gi, '');
   //TODO: add accessiblity language
   if(violationType.toLowerCase() === 'red'){
     violationIcon = 'fa-color-danger fa-exclamation-circle';
+    violationStr = (<strong>{violationStr}</strong>);
   } else if(violationType.toLowerCase() === 'blue'){
     violationIcon = 'fa-color-info fa-cog';
   }
   return (
     <tr className={"collapse " + index} key={index}>
-      <td colSpan="2" className="p-l-lg"><span className={'fa ' + violationIcon} aria-hidden="true"/> {violation.violationDescr.replace(/[^\w\s]/gi, '')} </td>
+      <td colSpan="2" className="p-l-lg"><span className={'fa ' + violationIcon} aria-hidden="true"/> {violationStr} </td>
       <td colSpan="1">{violation.violationPoints}</td>
     </tr>
   );
