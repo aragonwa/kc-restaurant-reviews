2950dc137c5e680a391d6cdbff04c3a637949dad
Alex Aragon <aragonwa@gmail.com>
2017-08-11 09:53:19 -0700

    Adding new actions for searching by city, zip, name

--- src/actions/restaurantReviewsActions.js
+++ src/actions/restaurantReviewsActions.js
@@ -1,5 +1,5 @@
 import * as types from '../constants/actionTypes';
-import {getRestaurantsApi} from '../api/api';
+import {getRestaurantsApi, getRestaurantsByNameApi, getRestaurantsByCityApi, getRestaurantsByZipApi} from '../api/api';
 
 export function updateFilter(value) {
   return {
@@ -56,6 +56,73 @@ export function loadRestaurantsFail(isLoading, error) {
   };
 }
 
+export function searchingRestaurantsByName(searchIsLoading) {
+  return {
+    type: types.SEARCHING_RESTAURANTS_BY_NAME,
+    searchIsLoading
+  };
+}
+
+export function searchingRestaurantsByNameSuccess(restaurants, searchIsLoading) {
+  return {
+    type: types.SEARCHING_RESTAURANTS_BY_NAME_SUCCESS,
+    restaurants,
+    searchIsLoading
+  };
+}
+
+export function searchingRestaurantsByNameFail(searchIsLoading, error) {
+  return {
+    type: types.SEARCHING_RESTAURANTS_BY_NAME_FAIL,
+    searchIsLoading,
+    error
+  };
+}
+export function searchingRestaurantsByCity(searchIsLoading) {
+  return {
+    type: types.SEARCHING_RESTAURANTS_BY_CITY,
+    searchIsLoading
+  };
+}
+
+export function searchingRestaurantsByCitySuccess(restaurants, searchIsLoading) {
+  return {
+    type: types.SEARCHING_RESTAURANTS_BY_CITY_SUCCESS,
+    restaurants,
+    searchIsLoading
+  };
+}
+
+export function searchingRestaurantsByCityFail(searchIsLoading, error) {
+  return {
+    type: types.SEARCHING_RESTAURANTS_BY_CITY_FAIL,
+    searchIsLoading,
+    error
+  };
+}
+export function searchingRestaurantsByZip(searchIsLoading) {
+  return {
+    type: types.SEARCHING_RESTAURANTS_BY_ZIP,
+    searchIsLoading
+  };
+}
+
+export function searchingRestaurantsByZipSuccess(restaurants, searchIsLoading) {
+  return {
+    type: types.SEARCHING_RESTAURANTS_BY_ZIP_SUCCESS,
+    restaurants,
+    searchIsLoading
+  };
+}
+
+export function searchingRestaurantsByZipFail(searchIsLoading, error) {
+  return {
+    type: types.SEARCHING_RESTAURANTS_BY_ZIP_FAIL,
+    searchIsLoading,
+    error
+  };
+}
+
 export function loadRestaurants(){
   return function(dispatch){
     dispatch(loadingRestaurants(true));
@@ -63,6 +130,37 @@ export function loadRestaurants(){
       dispatch(loadRestaurantsSuccess(response, false));
     }).catch(error => {
       dispatch(loadRestaurantsFail(false, error));
+    });
+  };
+}
+
+export function searchRestaurants(name){
+  return function(dispatch){
+    dispatch(searchingRestaurantsByName(true));
+    return getRestaurantsByNameApi(name).then((response) => {
+      dispatch(searchingRestaurantsByNameSuccess(response, false));
+    }).catch(error => {
+      dispatch(searchingRestaurantsByNameFail(false, error));
+    });
+  };
+}
+export function searchCity(city){
+  return function(dispatch){
+    dispatch(searchingRestaurantsByCity(true));
+    return getRestaurantsByCityApi(city).then((response) => {
+      dispatch(searchingRestaurantsByCitySuccess(response, false));
+    }).catch(error => {
+      dispatch(searchingRestaurantsByCityFail(false, error));
+    });
+  };
+}
+export function searchZip(zip){
+  return function(dispatch){
+    dispatch(searchingRestaurantsByZip(true));
+    return getRestaurantsByZipApi(zip).then((response) => {
+      dispatch(searchingRestaurantsByZipSuccess(response, false));
+    }).catch(error => {
+      dispatch(searchingRestaurantsByZipFail(false, error));
     });
   };
 }
