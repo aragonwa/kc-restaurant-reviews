518608d3c673854cb6d944237dcd39d0d5c2a931
Alex Aragon <aragonwa@gmail.com>
2017-08-11 09:52:03 -0700

    Updating apis to include new endpoints

--- src/api/api.js
+++ src/api/api.js
@@ -3,10 +3,7 @@ import RestaurantUrls from '../constants/appSettings';
 const urlApi = RestaurantUrls.setEnvironment(process.env.NODE_ENV);
 
 export function getRestaurantsApi () {
-  const url = urlApi.fullList;
-
-  // return new Promise(function (resolve, reject) {
-  return fetch(url)
+  return fetch(urlApi.fullList)
     .then(response => {
       if (response.ok) {
         return response.json();
@@ -20,39 +17,10 @@ export function getRestaurantsApi () {
     .catch(err => {
       return Promise.reject(Error('Network Error: ' + err));
     });
-
-    // Do the usual XHR stuff
-    // let req = new XMLHttpRequest()
-    // req.open('GET', url)
-
-    // req.onload = function () {
-    //   // This is called even on 404 etc
-    //   // so check the status
-    //   if (req.status == 200) {
-    //     // Resolve the promise with the response text
-    //     resolve(JSON.parse(req.response))
-    //   }else {
-    //     // Otherwise reject with the status text
-    //     // which will hopefully be a meaningful error
-    //     reject(Error(req.statusText))
-    //   }
-    // }
-
-    // // Handle network errors
-    // req.onerror = function () {
-    //   reject(Error('Network Error'))
-    // }
-
-// // Make the request
-// req.send()
-// })
 }
 
 export function getBusinessApi (id) {
-  const url = urlApi.business + id;
-
-  // return new Promise(function (resolve, reject) {
-  return fetch(url)
+  return fetch(urlApi.business + id)
     .then(response => {
       if (response.ok) {
         return response.json();
@@ -66,38 +34,10 @@ export function getBusinessApi (id) {
     .catch((err) => {
       return Promise.reject(Error('Network Error: ' + err));
     });
-    // Do the usual XHR stuff
-    //   let req = new XMLHttpRequest()
-    //   req.open('GET', url)
-
-    //   req.onload = function () {
-    //     // This is called even on 404 etc
-    //     // so check the status
-    //     if (req.status == 200) {
-    //       // Resolve the promise with the response text
-    //       resolve(JSON.parse(req.response))
-    //     }else {
-    //       // Otherwise reject with the status text
-    //       // which will hopefully be a meaningful error
-    //       reject(Error(req.statusText))
-    //     }
-    //   }
-
-    //   // Handle network errors
-    //   req.onerror = function () {
-    //     reject(Error('Network Error'))
-    //   }
-
-//   // Make the request
-//   req.send()
-// })
-// .catch( e =>{Error(e.statusText);})
-};
+}
 
 export function getInspectionsApi (id) {
-  const url = urlApi.inspections + id;
-
-  return fetch(url)
+  return fetch(urlApi.inspections + id)
     .then(response => {
       if (response.ok) {
         return response.json();
@@ -111,31 +51,52 @@ export function getInspectionsApi (id) {
     .catch(err => {
       return Promise.reject(Error('Network Error: ' + err));
     });
-
-  // return new Promise(function (resolve, reject) {
-  //   // Do the usual XHR stuff
-  //   let req = new XMLHttpRequest();
-  //   req.open('GET', url);
-
-  //   req.onload = function () {
-  //     // This is called even on 404 etc
-  //     // so check the status
-  //     if (req.status == 200) {
-  //       // Resolve the promise with the response text
-  //       resolve(JSON.parse(req.response));
-  //     }else {
-  //       // Otherwise reject with the status text
-  //       // which will hopefully be a meaningful error
-  //       reject(Error(req.statusText));
-  //     }
-  //   };
-
-  //   // Handle network errors
-  //   req.onerror = function () {
-  //     reject(Error('Network Error'));
-  //   };
-
-  //   // Make the request
-  //   req.send();
-  // });
-};
+}
+export function getRestaurantsByNameApi (name) {
+  return fetch(urlApi.searchName + name)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        return Promise.reject(Error(response.status));
+      }
+    })
+    .then(data => {
+      return Promise.resolve(data);
+    })
+    .catch(err => {
+      return Promise.reject(Error('Network Error: ' + err));
+    });
+}
+export function getRestaurantsByCityApi (city) {
+  return fetch(urlApi.searchCity + city)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        return Promise.reject(Error(response.status));
+      }
+    })
+    .then(data => {
+      return Promise.resolve(data);
+    })
+    .catch(err => {
+      return Promise.reject(Error('Network Error: ' + err));
+    });
+}
+export function getRestaurantsByZipApi (zip) {
+  return fetch(urlApi.searchZip+ zip)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        return Promise.reject(Error(response.status));
+      }
+    })
+    .then(data => {
+      return Promise.resolve(data);
+    })
+    .catch(err => {
+      return Promise.reject(Error('Network Error: ' + err));
+    });
+}
